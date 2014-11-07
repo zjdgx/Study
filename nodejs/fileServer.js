ac09c49ab3bbf655b3f27456cd2161f4633af4b8
zdjgx <zjdgx20131030@163.com>
2014-11-07 11:46:49 +0800

    learnyounode: 	time server 	HTTP file server 	http uppercaserver: through2-map 	http json api server

---
+++ nodejs/fileServer.js
@@ -0,0 +1,17 @@
+﻿/**
+ * create on 2014/11/07 nhn
+ *   node fileServer.js 8000 "F:/zjdgx/Study/nodejs/sa.txt"
+ *    file server: response.writeHead(200,{"content-type":"text/plain"})
+ *		the second parameter must be a native file, can't be url.
+ */
+var http = require("http"),
+	fs = require("fs");
+
+var server = http.createServer(function(req, res) {
+	res.writeHead(200, { 'content-type': 'text/plain' });
+	var data = fs.createReadStream(process.argv[3],{encoding:"utf8"});
+	data.pipe(res);
+	console.log("finished");
+});
+
+server.listen(Number(process.argv[2]));
---
+++ nodejs/timeServer.js
@@ -0,0 +1,17 @@
+﻿var url = require("url"),
+	http = require("http");
+	
+var server = http.createServer(function (req, res) {
+	var json = url.parse(req.url, true), time = {}, date = new Date(json.query.iso);
+	if (json.pathname === "/api/parsetime") {
+		time["hour"] = date.getHours();
+		time["minute"] = date.getMinutes();
+		time["second"] = date.getSeconds();
+		return res.end(JSON.stringify(time));
+	}
+	if (json.pathname === "/api/unixtime") {
+		res.end(JSON.stringify({"unixtime":date.getTime()}));
+	}
+});
+
+server.listen(Number(process.argv[2]));
