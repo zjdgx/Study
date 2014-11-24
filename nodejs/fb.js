cbb101812060f77cf4ac1a095ed5c3521af2443a
zjdgx <jianggang@nhn.com>
2014-11-24 17:16:40 +0800

    nodejs in Action

---
+++ nodejs/fb.js
@@ -0,0 +1,15 @@
+﻿// 斐波拉切数列测试
+
+var moment = require("moment"),
+	start = moment(),
+	result = 0;
+
+function fb( n ) {
+	if( n == 0 || n == 1 ) {
+		return 1;
+	} else {
+		return fb(n-1)+fb(n-2);
+	}
+}
+result = fb(40);
+console.log("time: "+(moment()-start)+", result: "+result);
