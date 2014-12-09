0cd9dc9f3c6dd0f1c9b0f5647796b9506d2e023e
jianggang <jianggang@nhn.com>
2014-12-09 18:01:59 +0800

    ppt

--- PPT/node-demo/zjdgx/processDemo.js
+++ PPT/node-demo/zjdgx/processDemo.js
@@ -2,19 +2,26 @@
  * Created by nhn on 2014/12/9.
  */
 
+var str = "",
+	fs = require("fs");
+
 process.stdin.setEncoding('utf8');
 
-process.stdin.on('readable', function() {
-    var chunk = process.stdin.read();
-    if (chunk !== null) {
-        process.stdout.write('data: ' + chunk);
-    }
+process.stdin.on('readable', function () {
+	var chunk = process.stdin.read();
+	if (chunk !== null) {
+		str += chunk;
+		process.stdout.write('data: ' + chunk);
+	}
 });
 
-process.stdin.on('end', function() {
-    process.stdout.write('end');
+process.stdin.on('end', function () {
+	fs.write('zjdgx.process.log',str,function(err){
+		console.log("file write failed...");
+	});
+	process.stdout.write('end');
 });
 
 process.on('SIGINT', function () {
-   console.log("process exit...");
+	console.log("process exit...");
 });
