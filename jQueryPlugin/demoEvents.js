7dced5d9ed43f3070a6234539a8e0c2c1dcce3fe
jianggang <jianggang@nhn.com>
2015-03-19 19:02:35 +0900

    jQuery Plugin Demos

--- jQueryPlugin/demoEvents.js
+++ jQueryPlugin/demoEvents.js
@@ -4,6 +4,51 @@ $('#thiskeyword').click(function(){
 $('#chainCall').click(function(){
 	$(this).pinkify().css('font-size', '32px');
 });
-$('').click(function(){
-	$(this).bestPluginEverDefaults({width: 200});
+$('#widgetTextbox').textboxdecorator();
+
+$('.seeCode').click(function(){
+	var $li = $(this).parents('li').find('h1');
+		code = $(this).parents('li').find('.prettyprint').clone();
+	$(this).parents('li').find('.prettyprint').dialog({title: $li.html(), width: 800, height: 400, close: function(event){
+		$li.after(code);
+	}});
+});
+
+var tbar;
+$('#progressbarTest').click(function(){
+	var t = $(this);
+	if ($('.progressbarTest').hasClass('progressbar')) {
+		tbar.progressbar('option', 'value', tbar.progressbar('value')+10);
+	} else {
+		tbar = $('.progressbarTest').progressbar({
+			value: 10, 
+			hide: { 
+				effect: "explode",
+				duration: 1000
+			}}).bind('progressbarcomplete', function(event, data){
+				console.log('complete....');
+				tbar.progressbar('option', 'hide', { effect: "explode", duration: 1000 });
+			});
+	}
+});
+
+$.zjdgx.progressbar.prototype.reset = function() {
+	this._setOption('value',0);
+}
+
+/** widget extension
+--------------------*/
+$('.widgetExtend').click(function(){
+	$(this).next().dialog({title:'widget继承测试'}).dialog('red');
+});
+
+
+/** widget _supper _supperApply
+------------------------------*/
+$('#supper').click(function(){
+	$('#_supperResult p:first').suppercompute().suppercompute('compute', 1, 2);
+});
+
+$('#supperSub').click(function(){
+	$('#_supperResult p:last').subcompute().subcompute('compute',1,2,3);
 });
---
+++ jQueryPlugin/jquery-plugin-demo.js
@@ -0,0 +1,169 @@
+﻿/**
+ * this keyword in plugin
+ */
+;(function($){
+	$.fn.pinkify = function() {
+		// this引用的是jQuery本身
+		return this.each(function() {
+			// 在循环体内，this关键字引用的是DOM元素
+			$(this).css({
+				'background-color': '#fe57a1',
+				'color': '#fff',
+				'text-shadow': 'none'
+			});
+		});
+	}
+})(jQuery)
+/**
+ * set options for plugin
+ */
+;(function($){
+	$.fn.bestPluginEver = function(options) {
+		var settings = $.extend({
+				'color': '#fff',
+				'background-color': '#fe57a1'
+			}, options);
+		
+		return this.css(settings);
+	}
+})(jQuery)
+/**
+ * set `defaults` to be public for plugin
+ */
+;(function($){
+	$.fn.bestPluginEverDefaults = function(options) {
+		var settings = $.extend({}, $.fn.bestPluginEverDefaults.defaults, options);
+		
+		return this.each(function(){
+			$(this).css('width', settings.width);
+		});
+	};
+	
+	$.fn.bestPluginEverDefaults.defaults = {
+		width: 100
+	};
+})(jQuery)
+
+/**
+ * 保持私有函数的私有性
+ */
+;(function($, window, document, undefined){
+	$.fn.dataCruncher = function(options) {
+		//插件之外不可访问
+		function privateSort( data ) {
+			// 私有方法
+		}
+		return this.each(function() {
+			var data = privateSort( data );
+		});
+	}
+})(jQuery, window, document)
+
+/*==========================jQuery widget==========================*/
+/* progress bar
+--------------------*/
+$.widget('zjdgx.progressbar', {
+	// 重写_create方法
+	_create: function() {
+		var progress = this.options.value + '%',
+			progressText = '<div class="curRate" style="width: '+this.options.value+'%;">'+progress+'</div>';
+		
+		this.element.addClass('progressbar').append(progressText);
+	},
+	value: function (value) {
+		if (value) {
+			if (value > 100) {
+				value = 10;
+			}
+			
+			if (value < 0) {
+				value = 0;
+			}
+			
+			this.options.value = value;
+			this.element.find('.curRate').css('width', value+'%').html(value+'%');
+			// 提供回调功能让用户进行扩展
+			if (this.options.value == 100) {
+				this._trigger('complete', null, {value: 100});
+			}
+			//**事件类型则是由插件名称和回调名称合并而成(progressbarcomplete)。回调和事件被触发时会收到同样的两个参数：事件对象和相关数据
+		} else {
+			return this.options.value;
+		}
+	},
+	// 当参数被修改时执行一些操作
+	_setOption: function (key, value) {
+		this.options[key] = value;
+		if (key === 'value')
+			this.value(value);
+	}
+});
+
+/* widget extension
+--------------------*/
+$.widget('zjdgx.dialog', $.ui.dialog, {
+	red: function() {
+		var e = this.element;
+		
+		setTimeout(function(){
+			e.css('color', 'red');
+		},1000);
+	}
+});
+
+/* widget _supper _supperApply
+-------------------------------*/
+$.widget('zjdgx.suppercompute', {
+	compute: function(a, b) {
+		this.element.html(a+b);
+		console.log('suppercompute: '+ (a+b));
+	}
+});
+$.widget('zjdgx.subcompute', $.zjdgx.suppercompute, {
+	compute: function(a, b, c) {
+		console.log('subcompute called suppercompute...');
+		this._super(a, b);
+		this.element.html(a+b+c);
+		console.log('subcompute: '+ (a+b+c));
+	}
+});
+
+/* textboxdecorator
+--------------------*/
+$.widget('ui.textboxdecorator', {
+	options: {},
+	_init: function(){  
+		//验证是否是需要装饰的元素 
+		
+		//this.element也就是调用此widget的元素  
+		var e = this.element,
+			tg = e[0].tagName.toLowerCase(),
+			ty = e.attr("type").toLowerCase();
+		  
+		if (!(tg === "input" || tg === "textarea")) {  
+			return;  
+		}  
+		if (!(ty === "text" || ty === "password")) {  
+			if (tg === "input")   
+				return;  
+		}  
+		
+		//ui-widget widget基本的样式，ui-state-default。默认状态的样式；ui- corner-all 圆角（基于css3，ie下不起作用）  
+		e.addClass("ui-widget ui-state-default ui-corner-all");  
+		//添加hover效果和active效果  
+		e.mouseover(function(){  
+			e.addClass("ui-state-hover");  
+		}).mouseout(function(){  
+			e.removeClass("ui-state-hover");  
+		}).mousedown(function(){  
+			e.addClass("ui-state-active");  
+		}).mouseup(function(){  
+			e.removeClass("ui-state-active");  
+		});  
+	},  
+	//销毁时，移除widget增加的样式  
+	destroy:function(){  
+		this.element.removeClass("ui-widget ui-state-default ui-corner-all ui-state-hover ui-state-active");  
+	}
+});
+/*==========================jQuery widget==========================*/
